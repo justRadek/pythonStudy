4d9719b8b1d212d0633fa50fbaa07fc20f389d5e
justRadek <31217520+justRadek@users.noreply.github.com>
2019-05-17 09:10:57 +0200

    Add files via upload

---
+++ faktorial.py
@@ -0,0 +1,29 @@
+def f(n):
+    assert n >= 0
+    answer = 1
+    while n > 1:
+        answer *= n
+        n -= 1
+    return answer
+
+def fact(n):
+    assert n >= 0
+    if n <= 1:
+        return n
+    else:
+        return n*fact(n - 1)
+
+def g(n):
+    x = 0
+    for i in range(n):
+        for j in range(n):
+            x += 1
+    return x
+
+def h(x):
+    assert type(x) == int and x >= 0
+    answer = 0
+    s = str(x)
+    for c in s:
+        answer += int(c)
+    return answer
